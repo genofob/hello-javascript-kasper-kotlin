36c8089f60b9b626b556c1ccaee82c7523b72054
genofob <kasper.geno@gmail.com>
2023-01-31 13:06:15 +0100

    Første commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " +
+    document.querySelector("#name").value +
+    " dit gamle røvhul på " +
+    document.querySelector("#age").value +
+    " med mail: " +
+    document.querySelector("#mail").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
